fe538383a47d979da2a7e54e435b0548d331e338
Mikl9 <Mikl9@DESKTOP-FGCPIFS>
2024-03-06 15:35:33 +0300

    Добавьте файлы проекта.

---
+++ Tamogotchi/Program.cs
@@ -0,0 +1,54 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите имя вашего Тамагочи:");
+        string name = Console.ReadLine();
+        Tamagotchi tamagotchi = new Tamagotchi(name);
+
+        while (tamagotchi.IsAlive())
+        {
+            tamagotchi.Hunger++;
+            tamagotchi.Tiredness++;
+            tamagotchi.Happiness--;
+
+            if (tamagotchi.Hunger >= 10 || tamagotchi.Tiredness >= 10)
+            {
+                tamagotchi.Health--;
+            }
+
+            tamagotchi.CheckStatus();
+
+            if (!tamagotchi.IsAlive())
+            {
+                Console.WriteLine($"{tamagotchi.Name} умер. Игра окончена.");
+                break;
+            }
+
+            Console.WriteLine("Что вы хотите сделать? (кормить, играть, спать, лечить, проверить статус)");
+            string action = Console.ReadLine().ToLower();
+
+            switch (action)
+            {
+                case "кормить":
+                    tamagotchi.Feed();
+                    break;
+                case "играть":
+                    tamagotchi.Play();
+                    break;
+                case "спать":
+                    tamagotchi.Sleep();
+                    break;
+                case "лечить":
+                    tamagotchi.Heal();
+                    break;
+                case "проверить статус":
+                    tamagotchi.CheckStatus();
+                    break;
+                default:
+                    Console.WriteLine("Неверное действие. Попробуйте еще раз.");
+                    break;
+            }
+        }
+    }
+}
---
+++ Tamogotchi/Tamogotchi.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+public class Tamagotchi
+{
+    public string Name { get; set; }
+    public int Health { get; set; } = 10;
+    public int Hunger { get; set; } = 0;
+    public int Tiredness { get; set; } = 0;
+    public int Happiness { get; set; } = 10;
+
+    public Tamagotchi(string name)
+    {
+        Name = name;
+    }
+
+    public void Feed()
+    {
+        if (Hunger > 0)
+        {
+            Hunger--;
+        }
+        else
+        {
+            Health--;
+        }
+        Happiness++;
+    }
+
+    public void Play()
+    {
+        if (Tiredness < 10)
+        {
+            Tiredness++;
+            Hunger++;
+            Happiness++;
+        }
+        else
+        {
+            Health--;
+        }
+    }
+
+    public void Sleep()
+    {
+        Tiredness = 0;
+        Health++;
+        Hunger++;
+        Happiness++;
+    }
+
+    public void Heal()
+    {
+        if (Health < 10)
+        {
+            Health++;
+            Happiness++;
+        }
+    }
+
+    public void CheckStatus()
+    {
+        Console.WriteLine($"Имя: {Name}");
+        Console.WriteLine($"Здоровье: {Health}");
+        Console.WriteLine($"Голод: {Hunger}");
+        Console.WriteLine($"Усталость: {Tiredness}");
+        Console.WriteLine($"Счастье: {Happiness}");
+    }
+
+    public bool IsAlive()
+    {
+        return Health > 0 && Hunger < 10 && Tiredness < 10;
+    }
+}
